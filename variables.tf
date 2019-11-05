2b09c62dc41b8e930aed95bdfd5c5126d370fbc6
William Hearn <sylus1984@gmail.com>
2019-11-05 11:35:23 -0500

    feat(opa): Add Azure Policy

--- variables.tf
+++ variables.tf
@@ -1,6 +1,14 @@
 variable "kubectl_service_account" {}
 variable "kubectl_namespace" {}
 
+variable "helm_service_account" {}
+
+variable "helm_namespace" {}
+
+variable "helm_repository" {}
+
+variable "chart_version" {}
+
 variable "dependencies" {
   type = "list"
 }
