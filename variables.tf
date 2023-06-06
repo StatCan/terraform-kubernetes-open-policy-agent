e5194329b4ae1f24c7631aeb34b9006d6c902261
Justin Bertrand <justin.bertrand@canada.ca>
2023-06-06 13:08:40 +0000

    feat: update to chart 3.12.0 and added variables for extra configurability.

--- variables.tf
+++ variables.tf
@@ -28,12 +28,19 @@ variable "chart" {
 }
 
 variable "chart_version" {
-  default = "3.8.1"
+  default = "3.12.0"
 }
 
 variable "values" {
   default = ""
   type    = string
+}
+
+variable "audit_match_kind_only" {
+  description = "Only check resources of the kinds specified in all constraints defined in the cluster."
+  type        = bool
+  default     = true
+  nullable    = false
 }
 
 variable "replicas" {
@@ -61,10 +68,22 @@ variable "opa_requests_memory" {
   type    = string
 }
 
+variable "log_level" {
+  description = "The log level for all components."
+  type        = string
+  default     = "WARNING"
+  nullable    = false
+}
+
 variable "image_hub" {
   default     = "docker.io/"
   type        = string
   description = "The name of the hub from which images will be pulled (with trailing slash)."
+
+  validation {
+    condition     = endswith(var.image_hub, "/")
+    error_message = "image_hub must end with a trailing slash"
+  }
 }
 
 variable "image_pull_secrets" {
@@ -72,6 +91,7 @@ variable "image_pull_secrets" {
   default     = []
   description = "The names of the ImagePullSecrets that the ServiceAccount will have access to."
 }
+
 variable "opa_audit_limits_cpu" {
   default = "1000m"
   type    = string
@@ -90,4 +110,52 @@ variable "opa_audit_requests_cpu" {
 variable "opa_audit_requests_memory" {
   default = "256Mi"
   type    = string
+}
+
+variable "exempt_namespaces" {
+  description = "The namespaces for which policies should not apply."
+  type        = list(string)
+  default     = []
+}
+
+variable "node_selector" {
+  type = map(string)
+  default = {
+    "kubernetes.io/os" = "linux"
+  }
+
+  description = "The nodeSelectors to apply to all components."
+}
+
+variable "tolerations" {
+  description = "The tolerations to set on all components."
+  type = list(object({
+    effect             = optional(string)
+    key                = optional(string)
+    operator           = optional(string)
+    toleration_seconds = optional(number)
+    value              = optional(string)
+    })
+  )
+  default = []
+
+  validation {
+    condition     = alltrue([for t in var.tolerations : t.effect == null ? true : contains(["", "NoSchedule", "PreferNoSchedule", "NoExecute"], t.effect)])
+    error_message = "Valid effects are one of: [null, \"\", NoSchedule, PreferNoSchedule, NoExecute]"
+  }
+
+  validation {
+    condition     = alltrue([for t in var.tolerations : t.operator == null ? true : contains(["", "Equals", "Exists"], t.operator)])
+    error_message = "Valid operators are Exists and Equal."
+  }
+
+  validation {
+    condition     = alltrue([for t in var.tolerations : (t.key == "" || t.key == null) ? t.operator == "Exists" : true])
+    error_message = "If key is empty, operator must be Exists."
+  }
+
+  validation {
+    condition     = alltrue([for t in var.tolerations : t.operator == "Exists" ? t.value == "" || t.value == null : true])
+    error_message = "If the operator is Exists, the value should be empty, otherwise just a regular string."
+  }
 }
